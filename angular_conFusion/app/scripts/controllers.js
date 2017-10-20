bcb106c9e5e6d4d0b932923e30eff1edc2d50646
Sergi Sabate <sersajar@gmail.com>
2017-10-20 16:14:51 +0200

    w4 .get() method

--- angular_conFusion/app/scripts/controllers.js
+++ angular_conFusion/app/scripts/controllers.js
@@ -8,7 +8,13 @@ angular.module('confusionApp')
             $scope.filtText = '';
             $scope.showDetails = false;
 
-            $scope.dishes= menuFactory.getDishes();
+            $scope.dishes= [];
+            menuFactory.getDishes()
+            .then(
+                function(response) {
+                    $scope.dishes = response.data;
+                }
+            );
 
                         
             $scope.select = function(setTab) {
@@ -70,9 +76,14 @@ angular.module('confusionApp')
 
         .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
 
-            var dish= menuFactory.getDish(parseInt($stateParams.id,10));
-            
-            $scope.dish = dish;
+            $scope.dish = {};
+                        menuFactory.getDish(parseInt($stateParams.id,10))
+            .then(
+                function(response){
+                    $scope.dish = response.data;
+                    $scope.showDish=true;
+                }
+            );
             
         }])
 
@@ -97,8 +108,16 @@ angular.module('confusionApp')
         .controller('IndexController', ['$scope', 'menuFactory', 
             'corporateFactory', function($scope, menuFactory, corpfac){
 
-            $scope.fdish = menuFactory.getDish(0);
-            
+            $scope.dish = {};
+
+            menuFactory.getDish(0)
+            .then(
+                function(response){
+                    $scope.dish = response.data;
+                    $scope.showDish = true;
+                }
+            );
+
             $scope.promotion = menuFactory.getPromotion(0);
                 
             $scope.leader = corpfac.getLeader(3);
